7a4b2b3431d02f5f298cf266cdec3f32aee7d0be
Dohyun Moon <d6moon@uwaterloo.ca>
2023-12-25 15:58:42 -0500

    fighter class and multithreading

---
+++ fighter.py
@@ -0,0 +1,11 @@
+class Fighter:
+    def __init__(self, name, height, losses, wins, strikes, takedowns, weight_class):
+        self.name = name
+        self.height = height
+        self.losses = losses
+        self.wins = wins
+        self.strikes = strikes
+        self.takedowns = takedowns,
+        self.weight_class = weight_class
+    
+    
--- main.py
+++ main.py
@@ -1,4 +1,33 @@
+import threading
+
 from ufc import get_fighter
 from pprint import pprint
+from fighter import Fighter
 
-pprint(get_fighter("Jon Jones"))
+
+def get_fighter_info1():
+    try:
+        info1 = get_fighter("Jon Jones")
+        fighter1 = Fighter(info1["name"], info1["height"], info1["losses"], info1["wins"], info1["strikes"], info1["takedowns"], info1["weight_class"])
+        pprint(vars(fighter1))
+    except BaseException as e:
+        print("Fighter name is not valid")
+
+def get_fighter_info2():
+    try:
+        info2 = get_fighter("Dohyun Moon")
+        fighter2 = Fighter(info2["name"], info2["height"], info2["losses"], info2["wins"], info2["strikes"], info2["takedowns"], info2["weight_class"])
+        pprint(vars(fighter2))
+    except BaseException as e:
+        print("Fighter name is not valid")
+
+t1 = threading.Thread(target=get_fighter_info1)
+t2 = threading.Thread(target=get_fighter_info2)
+
+
+if __name__ == "__main__":
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
