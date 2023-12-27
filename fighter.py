e87bd5f9a7cfe98cfcfe4d3f185852c61e002768
Dohyun Moon <d6moon@uwaterloo.ca>
2023-12-26 19:25:52 -0500

    fighter class updated

--- fighter.py
+++ fighter.py
@@ -7,5 +7,12 @@ class Fighter:
         self.strikes = strikes
         self.takedowns = takedowns,
         self.weight_class = weight_class
-    
-    
+        self.takedown_defence_rate = takedowns[0]["takedown defense"] / (wins["total"] + losses["total"])
+        
+
+    def _total_strikes(self):
+        total = 0
+        for key, value in self.strikes:
+            if key != "attempted" and key != "strikes per minute" and key != "striking defense":
+                total += value
+        return total
