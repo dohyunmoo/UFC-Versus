41e26db6d13535f3b36aaa4a6e2fb0293c8af506
Dohyun Moon <d6moon@uwaterloo.ca>
2023-12-26 20:39:11 -0500

    simple gui added with black formatting

--- fighter.py
+++ fighter.py
@@ -5,14 +5,19 @@ class Fighter:
         self.losses = losses
         self.wins = wins
         self.strikes = strikes
-        self.takedowns = takedowns,
+        self.takedowns = (takedowns,)
         self.weight_class = weight_class
-        self.takedown_defence_rate = takedowns[0]["takedown defense"] / (wins["total"] + losses["total"])
-        
+        self.takedown_defence_rate = int(takedowns["takedown defense"]) / (
+            int(wins["total"]) + int(losses["total"])
+        )
 
     def _total_strikes(self):
         total = 0
         for key, value in self.strikes:
-            if key != "attempted" and key != "strikes per minute" and key != "striking defense":
-                total += value
+            if (
+                key != "attempted"
+                and key != "strikes per minute"
+                and key != "striking defense"
+            ):
+                total += int(value)
         return total
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import threading
+import tkinter as tk
 
 from ufc import get_fighter
 from pprint import pprint
@@ -8,26 +9,69 @@ from fighter import Fighter
 def get_fighter_info1():
     try:
         info1 = get_fighter("Jon Jones")
-        fighter1 = Fighter(info1["name"], info1["height"], info1["losses"], info1["wins"], info1["strikes"], info1["takedowns"], info1["weight_class"])
+        fighter1 = Fighter(
+            info1["name"],
+            info1["height"],
+            info1["losses"],
+            info1["wins"],
+            info1["strikes"],
+            info1["takedowns"],
+            info1["weight_class"],
+        )
         pprint(vars(fighter1))
     except BaseException as e:
         print("Fighter name is not valid")
 
+
 def get_fighter_info2():
     try:
-        info2 = get_fighter("Dohyun Moon")
-        fighter2 = Fighter(info2["name"], info2["height"], info2["losses"], info2["wins"], info2["strikes"], info2["takedowns"], info2["weight_class"])
+        info2 = get_fighter("Brandon Moreno")
+        fighter2 = Fighter(
+            info2["name"],
+            info2["height"],
+            info2["losses"],
+            info2["wins"],
+            info2["strikes"],
+            info2["takedowns"],
+            info2["weight_class"],
+        )
         pprint(vars(fighter2))
     except BaseException as e:
         print("Fighter name is not valid")
 
-t1 = threading.Thread(target=get_fighter_info1)
-t2 = threading.Thread(target=get_fighter_info2)
+
+def main_gui():
+    root = tk.Tk()
+    root.title("UFC Matchup Predictor")
+
+    # Create and place widgets using grid
+    label1 = tk.Label(root, text="Fighter 1")
+    label1.grid(row=0, column=0)
+
+    label2 = tk.Label(root, text="Fighter 2")
+    label2.grid(row=0, column=1)
+
+    input1 = tk.Entry(root, text="Enter fighter1 name")
+    input1.grid(row=1, column=0, sticky="ew")
+
+    input2 = tk.Entry(root, text="Enter fighter2 name")
+    input2.grid(row=1, column=1, sticky="ew")
+
+    button1 = tk.Button(root, text="Button")
+    button1.grid(row=2, column=0, columnspan=2)  # Columnspan to span multiple columns
+
+    # Run the main loop
+    root.mainloop()
 
 
 if __name__ == "__main__":
-    t1.start()
-    t2.start()
+    # t1 = threading.Thread(target=get_fighter_info1)
+    # t2 = threading.Thread(target=get_fighter_info2)
 
-    t1.join()
-    t2.join()
+    # t1.start()
+    # t2.start()
+
+    # t1.join()
+    # t2.join()
+
+    main_gui()
